a04e8cdcde5dd48e7d900a70a1a10a7302bb5d9e
Drew Zarn <drew@abmp.com>
2018-09-14 15:53:05 -0600

    Begin moving to bootstrap

--- pages/addtransaction.php
+++ pages/addtransaction.php
@@ -1,20 +1,15 @@
-<h3>Add Transaction</h3>
+<h4>Add Transaction</h4>
 <form id="addtransaction" action="transaction/add" method="POST" autocomplete="off">
 	<input autocomplete="false" name="hidden" type="text" style="display:none;">
-	<label for="add_payee">Payee</label>
-	<input type="text" id="add_payee" name="add_payee" />
+	<input type="text" id="add_payee" name="add_payee" placeholder="Payee"/>
 
-	<label for="add_category">Category</label>
-	<input type="text" id="add_category" name="add_category" />
+	<input type="text" id="add_category" name="add_category" placeholder="Category" />
 
-	<label for="add_amount">Amount</label>
-	<input type="number" id="add_amount" name="add_amount" step="0.01" />
+	<input type="number" id="add_amount" name="add_amount" step="0.01" placeholder="Amount" />
 
-	<label for="add_date">Date</label>
 	<input type="date" id="add_date" name="add_date" placeholder="Date" />
 
-	<label for="add_description">Description</label>
-	<input type="text" id="add_description" name="add_description" />
+	<input type="text" id="add_description" name="add_description" placeholder="Description" />
 
 	<p class="message error"></p>
 

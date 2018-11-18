8bde3ff7d1b794db1c84411df46f8367b4b04d28
Drew Zarn <drew@abmp.com>
2018-11-17 20:41:02 -0700

    Don't compare balance sheet values to empty cells

--- js/rules.js
+++ js/rules.js
@@ -336,11 +336,11 @@ function loadBalances() {
             var $el = $(el);
             if ($el.text() != '-') {
                 var isAsset = $el.closest('tbody').data('accounttypeasset') == "1";
-                if ($el.data('amount') == $el.next().data('amount')) {
+                if ($el.data('amount') == $el.nextAll(':not(.empty)').filter(':first').data('amount')) {
                     $el.addClass('table-warning');
-                } else if ($el.data('amount') > $el.next().data('amount')) {
+                } else if ($el.data('amount') > $el.nextAll(':not(.empty)').filter(':first').data('amount')) {
                     $el.addClass(isAsset ? 'table-success' : 'table-danger');
-                } else if ($el.data('amount') < $el.next().data('amount')) {
+                } else if ($el.data('amount') < $el.nextAll(':not(.empty)').filter(':first').data('amount')) {
                     $el.addClass(isAsset ? 'table-danger' : 'table-success');
                 }
                 $el.text(currencyFormatter.format($el.text()));

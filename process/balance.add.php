19327658f0ea34a8b260b705e1be9ab6c48780af
Drew Zarn <drew@abmp.com>
2018-11-24 20:15:35 -0700

    Store balance parity in DB

--- process/balance.add.php
+++ process/balance.add.php
@@ -8,10 +8,16 @@ foreach($POSTDATA as $accountId => $amount) {
 	if(!isset($accounts[$accountId])) {
 		$accounts[$accountId] = r::load('account', $accountId);
 	}
+	$lastBalance = R::findOne('balance', 'account_id=? AND date<? ORDER BY date DESC LIMIT 1', [$accountId, $date]);
+	
 	$balance = R::dispense('balance');
 	$balance->date = $date;
 	$balance->amount = $amount;
 	$balance->account = $accounts[$accountId];
+	$balance->parity = 0;
+	if($lastBalance->amount != $amount){
+		$balance->parity = ($lastBalance->amount > $amount) ? -1 : 1;
+	}
 	R::store($balance);
 }
 

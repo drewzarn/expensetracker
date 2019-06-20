38a932b8a7ea4d17d9952873a103fa39aab7d6de
Drew Zarn <drew@abmp.com>
2019-06-20 15:37:41 -0600

    Make FontAwesome work again

--- pages/_head.php
+++ pages/_head.php
@@ -16,7 +16,7 @@
 	<script src="js/datatables.min.js"></script>
 	<script src="js/TransactionTable.Filter.js"></script>
 
-	<link rel="stylesheet" href="css/fontawesome.css"/>
+	<link rel="stylesheet" href="//use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous" />
 
 	<script src="js/jscharting.js"></script>
 

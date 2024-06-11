a51adb7d4b9d608b496218db75d9bae0ada27254
Drew Zarn <drew@abmp.com>
2024-06-11 14:39:05 -0600

    Reorder, include sankey tab

--- pages/index.php
+++ pages/index.php
@@ -3,4 +3,5 @@
 <?php include('tab-dashboard.php'); ?>
 <?php include('tab-transactions.php'); ?>
 <?php include('tab-balances.php'); ?>
+<?php include('tab-sankey.php'); ?>
 <?php include('tab-manage.php'); ?>

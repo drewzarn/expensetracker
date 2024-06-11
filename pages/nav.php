3064b8c25d5d2346b73ba8c3ad36120a351dd0d2
Drew Zarn <drew@abmp.com>
2024-06-11 14:31:23 -0600

    Sankey nav and tab

--- pages/nav.php
+++ pages/nav.php
@@ -2,6 +2,7 @@
 	<span class="navbar-brand d-none d-md-inline">Expenses</span>
     <a class="nav-link text-muted" href="#" data-toggle="modal" data-target="#modal_addtransaction"><span class="d-none d-md-inline">Add Transaction</span> <i class="fas fa-plus-square"></i></a>
 	<a class="nav-link text-muted" href="#dashboard"><span class="d-none d-md-inline">Dashboard</span> <i class="fas fa-tachometer-alt"></i></a>
+	<a class="nav-link text-muted" href="#sankey"><span class="d-none d-md-inline">Sankey</span> <i class="fas fa-chart-simple"></i></a>
 	<a class="nav-link text-muted" href="#transactions"><span class="d-none d-md-inline">Transactions</span> <i class="fas fa-file-invoice-dollar"></i></a>
 	<a class="nav-link text-muted" href="#balances"><span class="d-none d-md-inline">Balances</span> <i class="fas fa-balance-scale"></i></a>
 	<a class="nav-link text-muted" href="#manage"><span class="d-none d-md-inline">Manage</span> <i class="fas fa-cogs"></i></a>
---
+++ pages/tab-sankey.php
@@ -0,0 +1,9 @@
+<div id="content-sankey" class="container-fluid">
+	<div class="row">
+		<div class="col-12" id="sankey">
+			<div class="card my-3">
+				<h3 class="card-header">Sankey Charts</h3>
+			</div>
+		</div>
+	</div>
+</div>

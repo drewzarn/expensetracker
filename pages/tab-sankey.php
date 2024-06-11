d1116360b72f0f621ae159cbc29e766063bde489
Drew Zarn <drew@abmp.com>
2024-06-11 14:42:56 -0600

    Datepicker

--- pages/tab-sankey.php
+++ pages/tab-sankey.php
@@ -3,6 +3,10 @@
 		<div class="col-12" id="sankey">
 			<div class="card my-3">
 				<h3 class="card-header">Sankey Charts</h3>
+				<div class="input-daterange input-group ml-3">
+					<input type="text" class="input-sm form-control" id="sankeydatestart">
+					<span class="input-group-text">to</span>
+					<input type="text" class="form-control" id="sankeydateend"></div>
 			</div>
 		</div>
 	</div>
